b504924c6c07f11fb97c6811eda2ff0345dddcf9
buchannf <buchannf@dukes.jmu.edu>
2020-09-23 16:10:35 -0400

    Home.js changes

--- home.js
+++ home.js
@@ -3,12 +3,12 @@ carousel();
 
 function carousel() {
   let i;
-  let x = document.getElementsByClassName("slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  let slide = document.getElementsByClassName("slide");
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  if (slideIndex > slide.length) {slideIndex = 1}
+  slide[slideIndex-1].style.display = "block";
   setTimeout(carousel, 7000); 
 }
